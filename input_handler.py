6f4db06c5d9e976e331b53a551d7781bf479187b
Reynir Aron Magnússon <reyniraron@gmail.com>
2018-05-18 10:10:35 +0000

    Change serial port

--- input_handler.py
+++ input_handler.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 import serial
 import time
 """
@@ -7,7 +7,7 @@ by Reynir Aron
 Serial input handler
 """
 
-serial_port = "/dev/cu.usbmodem1461"
+serial_port = "/dev/cu.usbmodem1411441"
 baud = 9600
 
 arduino = serial.Serial(serial_port, baud, timeout=5)
