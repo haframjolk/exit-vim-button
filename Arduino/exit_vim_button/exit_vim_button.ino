70f9fe35861eb7d4032840dcc78b1511e390a465
Reynir Aron Magnússon <reyniraron@gmail.com>
2018-05-18 10:38:50 +0000

    Make sketch write message on button press

--- Arduino/exit_vim_button/exit_vim_button.ino
+++ Arduino/exit_vim_button/exit_vim_button.ino
@@ -4,11 +4,20 @@
    by Reynir Aron
    Arduino sketch
 */
+const int BUTTON = 7;        // Pin no. of pushbutton
+const int ACTIVE = LOW;      // Button is active low
+const int PUSH_DELAY = 1000; // Cooldown in ms between button presses
+const char MSG = '1';        // Message to write when button is pressed
+int button_state;
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    Serial.println("0");
+    button_state = digitalRead(BUTTON);
+    if (button_state == ACTIVE) {
+        Serial.println(MSG);
+        delay(PUSH_DELAY);
+    }
 }
